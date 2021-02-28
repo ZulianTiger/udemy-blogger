1e16f614a1bde916b260197f42fe48cf2bab68d0
ady0606 <ady0606@gmail.com>
2021-02-28 19:20:39 +0100

    navbar basic styling

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,29 +1,37 @@
+//Styles
+import styles from '../../styles/navbar.module.css';
+
 //Next.js
 import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <div>
-            <Link href={'/'}>
-                <a>
-                    <h1>Home</h1>
-                </a>
-            </Link>
-            <Link href={'/profile'}>
-                <a>
-                    <h1>Profile</h1>
-                </a>
-            </Link>
-            <Link href={'new-post'}>
-                <a>
-                    <h1>New Post</h1>
-                </a>
-            </Link>
-            <Link href={'/login'}>
-                <a>
-                    <h1>Login</h1>
-                </a>
-            </Link>
+        <div class={styles.container}>
+            <div class={styles.logo}>
+                <h1>udemy-blogger</h1>
+            </div>
+            <div class={styles.navitems}>
+                <Link href={'/'}>
+                    <a>
+                        <h1>Home</h1>
+                    </a>
+                </Link>
+                <Link href={'/profile'}>
+                    <a>
+                        <h1>Profile</h1>
+                    </a>
+                </Link>
+                <Link href={'new-post'}>
+                    <a>
+                        <h1>New Post</h1>
+                    </a>
+                </Link>
+                <Link href={'/login'}>
+                    <a>
+                        <h1>Login</h1>
+                    </a>
+                </Link>
+            </div>
         </div>
     )
 }
